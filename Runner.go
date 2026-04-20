a89edb58c2cabe90aac80611cea4fd41a5627941
oyinda <akinwandeoyindamola3@gmail.com>
2026-04-20 10:37:22 +0100

    .

--- Runner.go
+++ Runner.go
@@ -11,15 +11,17 @@ func Runner(input string, result []string)  {
 		return
 	}
 	words := strings.Split( input,"\\n")
-	for _, word := range words {
+	for w, word := range words {
 		if word == ""{
-			fmt.Println()
+			if w != 0 {
+				fmt.Println()
+			}
 			continue
 		}
 		for i := 0; i < 8; i++ {
 			for _, char := range word {
 				if char < 32 || char > 126 {
-					continue
+					return
 				}
 				startLine := (int(char)-32)* 9 + 1 
 				fmt.Print(result[startLine+i])			
